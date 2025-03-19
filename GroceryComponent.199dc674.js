2c7dc1a1246e7c69aed631a5411a8a729cde54b9
Daulat68 <someone@email.com>
2025-03-19 22:24:52 +0530

    Updates

---
+++ GroceryComponent.199dc674.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire94c2;(0,e.register)("1ZzD8",function(r,i){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>l,set:void 0,enumerable:!0,configurable:!0});var o=e("ayMG0"),l=()=>(0,o.jsxs)("div",{children:[(0,o.jsx)("h1",{children:"Our Grocery Online Store"}),(0,o.jsx)("p",{children:"We have a lot of child components inside this webpage!"})]})});
+//# sourceMappingURL=GroceryComponent.199dc674.js.map
